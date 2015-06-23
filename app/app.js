75488d1105708fdf89acfa670ed3447a7fe25490
RedEyeRon <BizGitHub@planetredeye.com>
2015-06-23 07:53:10 -0700

    ReorderToWebstormLayout

---
+++ app/app.js
@@ -0,0 +1,47 @@
+(function () {
+    var app = angular.module('myApp', ['ngPopup', 'coord', 'simplePlotting', 'ecefService']);
+
+    //app.service('EcefService', function() {
+    //
+    //    this.method1 = function () {
+    //        alert("Yoda");
+    //    }
+    //});
+
+    app.directive('myMap', function (EcefService) {
+        // directive link function
+        var link = function (scope, element, attrs) {
+
+            // map config
+            var mapOptions = {
+                center: new google.maps.LatLng(33.32, -112.0),
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                scrollwheel: true,
+                rotateControl: true,
+                overviewMapControl: true,
+                mapTypeControl: true
+            };
+
+            // init the map
+            function initMap() {
+                if (scope.mainMap === void 0) {
+                    scope.mainMap = new google.maps.Map(element[0], mapOptions);
+                }
+            }
+
+            // show the map and place some markers
+            initMap();
+            EcefService.method1();
+            //window.prompt("Copy to clipboard: Ctrl+C, Enter", "Yoda");
+
+        };
+
+        return {
+            restrict: 'A',
+            template: '<div id="gmaps"></div>',
+            replace: true,
+            link: link
+        };
+    });
+})();
