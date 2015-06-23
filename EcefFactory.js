daf8815420a399c776f05db44d2c0e9a86f0084c
RedEyeRon <BizGitHub@planetredeye.com>
2015-06-23 07:37:56 -0700

    ReorderToWebstormLayout

--- EcefFactory.js
+++
@@ -1,9 +0,0 @@
-
-    var service = angular.module('ecefService', []);
-
-    service.service('EcefService', function () {
-
-        this.method1 = function () {
-            //alert("Yoda");
-        }
-    });
